e7a2f8169dda47a48fca7e1f59e23328d82fd60f
chenbj2333 <1749402844@qq.com>
2018-06-11 15:29:57 +0800

    fix warning

--- src/views/recommend/Recommend.vue
+++ src/views/recommend/Recommend.vue
@@ -60,7 +60,7 @@ export default {
     _getDiscList() {
       getDiscList().then(res => {
         if (res.code === ERR_OK) {
-          this.discList = res.data.list
+          this.discList = res.data.list;
         }
       });
     },
